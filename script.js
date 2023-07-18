a26efad34c586acb8a579530decf61510c1f95fa
CodeFlow0 <128867372+CodeFlow0@users.noreply.github.com>
2023-07-18 15:31:06 -0500

    Allow user to add a new item
    Added JS and Jquery to allow the user to add a new item to the shopping cart.

--- script.js
+++ script.js
@@ -24,4 +24,29 @@ $(document).ready(function () {
     });
     $('.total-price').text('$' + totalPrice.toFixed(2));
   }
+
+  // Allow user to add a new item
+  $('.create').click(function () {
+    const $row = $(this).closest('tr');
+    const itemName = $row.find('input[type="text"]').val();
+    const itemPrice = $row.find('input[type="number"]').val();
+    const newRow = createRow(itemName, itemPrice);
+    $row.before(newRow);
+
+    // Attach the event handler for the new item's quantity
+    newRow.find('.quantity input').on('change', updateSubtotal);
+
+    $row.find('input[type="text"]').val('');
+    $row.find('input[type="number"]').val('');
+  });
+
+  function createRow(itemName, itemPrice) {
+    const $row = $('<tr></tr>');
+    const $item = $('<td class="item"></td>').text(itemName);
+    const $price = $('<td class="price"></td>').text('$' + itemPrice + '.00');
+    const $quantity = $('<td class="quantity">QTY <input class="me-2" type="number" value="0"><button class="btn btn-danger cancel">Cancel</button></td>');
+    const $itemTotal = $('<td class="item-total">$0.00</td>');
+    $row.append($item, $price, $quantity, $itemTotal);
+    return $row;
+  }
 });
