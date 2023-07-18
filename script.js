2e47705352b5e338caa94037a1feb5ee54049b03
CodeFlow0 <128867372+CodeFlow0@users.noreply.github.com>
2023-07-18 15:14:55 -0500

    Calculate and display item totals
    Added JS to calculate both the subtotals of each item and the total of all groceries in the cart.

--- script.js
+++ script.js
@@ -1,81 +1,27 @@
-const updateMarketValue = function (ele) {
-  const sharesOwned = parseFloat($(ele).find('.shares input').val());
-  const marketPrice = parseFloat($(ele).find('.marketPrice input').val());
-  const marketValue = sharesOwned * marketPrice;
-  $(ele).children('.marketValue').text(marketValue);
-  return marketValue;
-}
-
-const updateUnrealizedProfit = function (ele, marketValue) {
-  const sharesOwned = parseFloat($(ele).find('.shares input').val());
-  const costPerShare = parseFloat($(ele).find('.cost input').val());
-  const costOfPurchase = sharesOwned * costPerShare;
-
-  const unrealizedProfit = marketValue - costOfPurchase;
-  $(ele).children('.profit').text(unrealizedProfit);
-  return unrealizedProfit;
-}
-
-const sum = function (acc, x) { return acc + x };
-
-const updatePortfolioValueAndProfit = function () {
-  let stocksMarketValues = [];
-  let stocksUnrealizedProfits = [];
-
-  $('tbody tr').each(function (i, ele) {
-    const marketValue = updateMarketValue(ele);
-    stocksMarketValues.push(marketValue);
-    const unrealizedProfit = updateUnrealizedProfit(ele, marketValue);
-    stocksUnrealizedProfits.push(unrealizedProfit);
-
-    const portfolioMarketValue = stocksMarketValues.reduce(sum);
-    const portfolioUnrealizedProfit = stocksUnrealizedProfits.reduce(sum);
-    $('#portfolioValue').text(portfolioMarketValue);
-    $('#portfolioProfit').text(portfolioUnrealizedProfit);
-  });
-}
-
 $(document).ready(function () {
-  updatePortfolioValueAndProfit();
+  // Calculate and display the sub total of each item 
+  $('.quantity input').on('change', updateSubtotal);
 
-  $(document).on('click', '.btn.remove', function (event) {
-    $(this).closest('tr').remove();
-    updatePortfolioValueAndProfit();
-  });
+  function updateSubtotal() {
+    const quantity = $(this).val();
+    const $row = $(this).closest('tr');
+    const price = getPrice($row);
+    const subTotal = (parseFloat(price) * parseInt(quantity)).toFixed(2);
+    $row.find('.item-total').text('$' + subTotal);
 
-  $(document).on('input', 'tr input', function () {
-    updatePortfolioValueAndProfit();
-  });
+    updateTotalPrice();
+  }
 
-  let timeout;
-  $('tr input').on('input', function () {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      updatePortfolioValueAndProfit();
-    }, 1000);
-  });
+  function getPrice($row) {
+    return $row.find('.price').text().replace('$', '');
+  }
 
-  $('#addStock').on('submit', function (event) {
-    event.preventDefault();
-    const name = $(this).children('[name=name]').val();
-    const shares = $(this).children('[name=shares]').val();
-    const cost = $(this).children('[name=cost]').val();
-    const marketPrice = $(this).children('[name=marketPrice]').val();
-
-    $('tbody').append('<tr>' +
-      '<td class="name">' + name + '</td>' +
-      '<td class="shares"><input type="number" value="' + shares + '" /></td>' +
-      '<td class="cost"><input type="number" value="' + cost + '" /></td>' +
-      '<td class="marketPrice"><input type="number" value="' + marketPrice + '" /></td>' +
-      '<td class="marketValue"></td>' +
-      '<td class="profit"></td>' +
-      '<td><button class="btn btn-light btn-sm remove">remove</button></td>' +
-      '</tr>');
-
-    updatePortfolioValueAndProfit();
-    $(this).children('[name=name]').val('');
-    $(this).children('[name=shares]').val('');
-    $(this).children('[name=cost]').val('');
-    $(this).children('[name=marketPrice]').val('');
-  });
+  function updateTotalPrice() {
+    let totalPrice = 0;
+    $('.item-total').each(function () {
+      const itemTotal = $(this).text().replace('$', '');
+      totalPrice += parseFloat(itemTotal);
+    });
+    $('.total-price').text('$' + totalPrice.toFixed(2));
+  }
 });
